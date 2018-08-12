8ea615cb093acba7c9b6e4cfcb77b7a13fb7ae37
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-08-11 22:01:50 -0300

    adicionado o usuario logado na home

--- src/main/java/com/curso/boot/demomvc/controller/HomeController.java
+++ src/main/java/com/curso/boot/demomvc/controller/HomeController.java
@@ -1,16 +1,32 @@
 package com.curso.boot.demomvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller		
+import com.curso.boot.demomvc.dao.UsuarioDAO;
+import com.curso.boot.demomvc.model.Usuario;
+
+@Controller
 public class HomeController {
 	
+	@Autowired
+	UsuarioDAO dao;
+
 	@RequestMapping("/")
-	public String home() {
+	public String home(ModelMap model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String login = auth.getName(); //obtém o usuario logado (login)
+		
+		Usuario usuario = dao.findById(login);  //busca o usuario
+		
+		model.addAttribute("username", usuario.getNome());  //mostra o nome do usuario
 		return "home";
 	}
-	
+
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
